f25b2965359faa7d7bccf8222cd30912e2b9356b
Armstrong <ammycan6@gmail.com>
2025-08-16 08:55:16 +0100

    TS global installations, Notes, and TypeScript Annotations

---
+++ tutorial.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Typescript Annotation
+// Data Type
+let phoneDigits = 2349009876543;
+let company = "Activison Blizzard";
+let isMale = true;
+let ageArr = [10, 34, 21];
+let personData1 = ["Kelvin", 21, true];
+let firstName = "Pedro";
+let personData2 = ["Kelvin", 21, true];
+// Function
+const concatenateValues = (a, b) => {
+    return a + b;
+};
+const addValues = (a, b) => {
+    return a + b;
+};
+console.log(concatenateValues("Hello", " World"));
+console.log(addValues(50, 10));
+console.log(concatenateValues("Hllo", " World"));
+console.log(addValues(20, 10));
+//# sourceMappingURL=tutorial.js.map
---
+++ tutorial.ts
@@ -0,0 +1,23 @@
+// Typescript Annotation
+// Data Type
+let phoneDigits: number = 2349009876543;
+let company: string = "Activison Blizzard";
+let isMale: boolean = true;
+let ageArr:number[] = [10, 34, 21];
+let personData1: (string | number | boolean)[] = ["Kelvin", 21, true];
+
+let firstName: any = "Pedro";
+let personData2: any[] = ["Kelvin", 21, true];
+
+// Function
+const concatenateValues = (a: string, b: string): string => {
+    return a + b;
+};
+
+const addValues = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log(concatenateValues("Hello", " World"));
+console.log(addValues(50, 10));
+
