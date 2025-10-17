dc7d26451295a29c33dc4d1e2b51772501781c4b
Armstrong <ammycan6@gmail.com>
2025-10-17 23:24:28 +0100

    js_inheritance

---
+++ js-to-ts-lessons/js_inheritance/index.ts
@@ -0,0 +1,111 @@
+// inheritance = allows a new class (child) to inherit properties and methods from an existing class (parent). Helps with code reusability (D-R-Y)
+
+class Animal {
+    alive: boolean = true;
+    name: string = "";
+
+    isAlive(): void {
+        console.log(`The ${this.name} ${this.alive ? "is" : "not"} alive.`);
+
+    }
+
+    eat(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is eating.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+
+    sleep(): void {
+        // Ternary/conditional statement
+        console.log(`This ${this.name} is ${this.alive ? "asleep" : "dead"}.`);
+    }
+}
+
+class Lion extends Animal {
+    name: string = "Lion";
+
+    run(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is running.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+
+    hunt(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is hunting.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+}
+
+class Dove extends Animal {
+    name: string = "Dove";
+
+    fly(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is flying.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+
+    forage(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is foraging.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+}
+
+class Shark extends Animal {
+    name: string = "Shark";
+
+    swim(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is swiming.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+
+    hunt(): void {
+        // if/conditional statement
+        if (this.alive) {
+            console.log(`This ${this.name} is hunting.`);
+        } else {
+            console.log(`The ${this.name} is dead.`);
+        }
+    };
+}
+
+
+// Usage
+// Lion
+const lion_1 = new Lion();
+lion_1.eat();
+lion_1.run();
+// lion_1.isAlive();
+// The lion is killed
+lion_1.alive = false;
+lion_1.eat();
+lion_1.isAlive();
+
+// Dove
+const dove_1 = new Dove();
+dove_1.fly();
+
+// Shark
+const shark_1 = new Shark();
+shark_1.swim();
